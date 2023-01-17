de1e1cd226696b3a7fa6041c2c7006b2bc00d62d
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2023-01-17 12:26:17 +0100

    Tipos

--- holaMundo/src/holaMundo/HolaMundo.java
+++ holaMundo/src/holaMundo/HolaMundo.java
@@ -1,10 +1,36 @@
 package holaMundo;
 
+/**
+ * Esta es la clase inicial del curso
+ * 
+ * @author Néstor
+ * @since 1.0
+ */
 public class HolaMundo {
 
-	public static void main(String[] args) {
-		
+	public static void ejemplosComentarios() {
+		/*
+		 * Este es mi primer programa Néstor 17/01/2023
+		 */
+		System.out.println("Hola Néstor");
 		System.out.println("Hola mundo");
+		System.out.println("Segunda línea");
+		System.out.println("Tercera línea");
+		System.out.println("Otra línea");
+
+	}
+	
+	private static void ejemplosTipos() {
+		int numero = 5;
+		int numero2 = 7;
+		int suma = numero + numero2;
+		System.out.println(suma);
+		
+
+	}
+
+	public static void main(String[] args) {
+		ejemplosComentarios();
 
 	}
 
