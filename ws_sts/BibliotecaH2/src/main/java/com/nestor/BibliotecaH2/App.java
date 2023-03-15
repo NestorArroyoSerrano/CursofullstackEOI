9325ea69ccd9f971de383b6614a6885ad36ef35c
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2023-03-15 08:12:36 +0100

    Comprobación de conexión

---
+++ ws_sts/BibliotecaH2/src/main/java/com/nestor/BibliotecaH2/App.java
@@ -0,0 +1,20 @@
+package com.nestor.BibliotecaH2;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+         Session session = sessionFactory.openSession();
+         if(session!=null) {
+      	   System.out.println("Sesión abierta correctamente");
+         } else {
+      	   System.out.println("Fallo al abrir la sesión");
+         }
+         
+    }
+}
