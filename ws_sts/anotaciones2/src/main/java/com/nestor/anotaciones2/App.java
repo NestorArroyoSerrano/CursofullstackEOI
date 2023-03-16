e2540a3c221ee975c3eb799c3fb87250152a1966
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2023-03-16 12:12:43 +0100

    anotaciones 2 abierto de sesiones

---
+++ ws_sts/anotaciones2/src/main/java/com/nestor/anotaciones2/App.java
@@ -0,0 +1,15 @@
+package com.nestor.anotaciones2;
+
+public class App 
+{
+	
+	
+	
+    public static void main( String[] args )
+    {
+    	java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.SEVERE);
+    	
+    	
+    	
+    }
+}
