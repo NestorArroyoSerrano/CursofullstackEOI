62a81439c5bc5982b5b8429a551ec77f350d84f0
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2023-03-22 10:50:13 +0100

    Spring mvc

---
+++ ws_sts/Spring-Mvc/src/test/java/com/nestor/springboot/backend/mvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestor.springboot.backend.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
