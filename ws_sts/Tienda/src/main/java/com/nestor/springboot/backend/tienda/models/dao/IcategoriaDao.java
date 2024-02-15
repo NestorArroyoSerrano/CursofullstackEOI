8aec27268640bfa4bb9d902a87028ba45dc6a462
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-02-15 13:13:20 +0100

    Delete ws_sts/Tienda directory

--- ws_sts/Tienda/src/main/java/com/nestor/springboot/backend/tienda/models/dao/IcategoriaDao.java
+++
@@ -1,8 +0,0 @@
-package com.nestor.springboot.backend.tienda.models.dao;
-
-import com.nestor.springboot.backend.tienda.models.entity.Categoria;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IcategoriaDao extends CrudRepository<Categoria, Integer> {
-	
-}
